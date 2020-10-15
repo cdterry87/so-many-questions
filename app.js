4c3518a2a2794c15d2dcb60619fdbcfc4150b24d
Jason Lynch <jasonl@adsisoftware.com>
2020-10-15 13:05:46 -0500

    Work on submit field validation

--- app.js
+++ app.js
@@ -12,6 +12,21 @@
 // Validate form input
 // - Input cannot be blank, must contain at least 3 words, and must end with a ?
 // - Return true or false if submit was valid or not
+function validateInput() {
+    const buttonElement = document.getElementById('submit');
+    var questionElement = document.getElementById('submitData').value;
+    buttonElement.addEventListener('click', function(event) {
+        console.log(questionElement.length);
+        if (questionElement.length < 3) {
+            console.log('value is less than 3');
+           // add error for value less that 3
+        }
+        if (questionElement == "") {
+            console.log('value is empty');
+            // add error for blank data
+        }
+    });
+}
 
 
 // Get a random answer

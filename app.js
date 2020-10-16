78945c0260ae3d55891182c164559908ccff5f7e
Harden T. Wallace II <hardentwallace@gmail.com>
2020-10-16 15:10:08 -0500

    Implemented initial 8 ball functionality

--- app.js
+++ app.js
@@ -1,12 +1,75 @@
-// Define an array of responses here
+// Possible Answers
+const answers = [
+    'Yes, absolutely!',
+    'Most definitely not!',
+    'It is unlikely.',
+    'It seems likely.',
+    'Not sure.',
+    'Try again later.'
+];
 
+// Error Handler
+const populateError = (message) => {
+    let errorOutput = document.getElementById('errorOutput');
+    errorOutput.innerText = message;
+};
+
+// Answer Handler
+const populateAnswer = (question) => {
+    // Getting Random Answer
+    let answer = answers[Math.floor(Math.random() * answers.length)];
+
+    // Injecting text content into the DOM
+    document.getElementById('questionResponse').innerText = question.value;
+    document.getElementById('answerResponse').innerText = answer;
+
+    // Clearing the question field
+    question.value = '';
+}
 
 // Handle the form submission
-// - Prevent page reload on submit
-// - Validate form
 // - Create an object for the response with the following properties: { question, answer, liked, date }
 // - Save to local storage (call)
 // - Render list of responses function (call)
+window.addEventListener('load', (event) => {
+    document.getElementById('questionForm').addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        // Clearing any previous error output
+        document.getElementById('errorOutput').innerText = '';
+
+        // Grabbing Inital Input Value
+        let question = document.getElementById('question');
+
+        // Validating against blank questions
+        if (question.value.trim() == '') {
+            populateError('No question was entered.');
+            return;
+        }
+
+        // Getting a mostly santized list of words
+        let words = question.value.trim().split(' ').filter((el) => {
+            return el != '';
+        });
+
+        // Validating against short questions
+        if (words.length < 3) {
+            populateError('You must have at least 3 words in your question.');
+            return;
+        }
+
+        // Getting Last Character of the typed question
+        let lastCharacter = question.value.trim().slice(-1);
+
+        // Validating that the last character is a question mark
+        if (lastCharacter != '?') {
+            populateError('Your question must end with a question mark.');
+            return;
+        }
+
+        populateAnswer(question);
+    });
+});
 
 
 // Validate form input

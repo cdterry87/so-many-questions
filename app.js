c5262f1744fddda65658511a4704c62e2501543c
Jason Lynch <jasonl@adsisoftware.com>
2020-10-21 13:42:53 -0500

    Complete Form Submission.

--- app.js
+++ app.js
@@ -7,75 +7,78 @@ let responses = [
     'Not sure. Try again later.',
 ];
 
-
 // Handle the form submission
 // - Prevent page reload on submit
-// - Validate form
-// - Create an object for the response with the following properties: { question, answer, liked, date }
-// - Save to local storage (call)
-// - Render list of responses function (call)
 var form = document.getElementById("questions");
 function handleForm(event) { event.preventDefault(); } 
 form.addEventListener('submit', handleForm);
 
+// - Validate form
+const submit = document.getElementById("submit");
+submit.addEventListener('click', function(event) {
+    if ( validate() ) {
+        randomAnswer();
+    }
+    document.forms[0].reset();
+});
+
+// - Create an object for the response with the following properties: { question, answer, liked, date }
+// - Save to local storage (call)
+// - Render list of responses function (call)
+
+
 // Validate form input
 // - Input cannot be blank, must contain at least 3 words, and must end with a ?
 // - Return true or false if submit was valid or not
-function validate() {
-    const buttonElement = document.getElementById('submit');
-    var questionElement = document.getElementById('submitData').value;
+function validate() 
+{
+    var question = document.getElementById('submitData').value;
+    document.getElementById("error").innerText = "";
     errorMsg = "Please type a valid question below, and don't forget the '?' at the end!"
 
-    buttonElement.addEventListener('click', function(event) {
-        if (questionElement.length < 3) {
-            // add error for value less that 3.
-            console.log('value is less than 3');
-            document.getElementById("error").innerHTML = errorMsg;
-            return false;
-        }else if (questionElement.value == "") {
-            // add error for blank data.
-            console.log('value is empty');
-            document.getElementById("error").innerHTML = errorMsg;
-            return false;
-        } else if (questionElement.endsWith("?") == false) {
-            // add error for questions not ending with question mark.
-            console.log('value is not a question.');
-            document.getElementById("error").innerHTML = errorMsg;
-            return false;
-        }
-        return true;
-    });
-}
+    if (question == "") {
+        // - Add error for blank data.
+        document.getElementById("error").innerText = errorMsg;
+        return false;
+    } else if (question.length < 3) {
+        // - Add error for value less that 3.
+        document.getElementById("error").innerText = errorMsg;
+        return false;
+    }else if (question.endsWith("?") == false) {
+        // - Add error for questions not ending with question mark.
+        document.getElementById("error").innerText = errorMsg;
+        return false;
+    }
 
+    return true;
+
+}
 
 // Get a random answer
 // - Define your random array of answers in this function and return the random answer
-function randomAnswer() {
-    document.getElementById("userQuestion").innerHTML = document.getElementById("submitData").value;
-    document.getElementById("submitData").value = "";
+let questions = [];
+function randomAnswer() 
+{
+    var question = document.getElementById("submitData").value;
     var randomResponse = responses[Math.floor(Math.random() * responses.length)];
     var response = document.getElementById("answer").innerText = randomResponse;
+
+    // Render answer below the form
+    document.getElementById("userQuestion").innerHTML = question;
 
     return response;
 }
 
-// Render answer below the form
-
 
 // Save responses in localstorage
 
-
 // Get responses from localstorage
-
 
 // Render list of responses
 
-
 // Toggle like
 
-
 // Delete response
-
 
 // Format date
 // - Format: YYYY-MM-DD @ HH:MM 
